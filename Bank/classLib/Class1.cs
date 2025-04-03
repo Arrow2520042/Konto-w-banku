ae71638899f63a2ba098122bdbe66bd54e3acd20
Piotr Wydymus <piotr.wydymus@microsoft.wsei.edu.pl>
2025-04-03 18:41:55 +0200

    create projects + classlib

---
+++ Bank/classLib/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace classLib
+{
+    public class Konto
+    {
+        private string klient;  //nazwa klienta
+        private decimal bilans;  //aktualny stan środków na koncie
+        private bool zablokowane = false; //stan konta
+
+        public Konto(string klient, decimal bilansNaStart = 0)
+        {
+            this.klient = klient;
+            this.bilans = bilansNaStart;
+        }
+
+        public Konto()
+        {
+            klient = "";
+            bilans = 0;
+        }
+    }
+
+    
+  
+}
